bf697c67d79442a10ce6b206986aa55f51c7d41d
Gazihan Alankus <gazialankus@gmail.com>
2019-05-05 02:48:20 +0300

    First try towards Google Test that failed.

--- src/TestedClass_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * TestedClass_test.cpp
- *
- *  Created on: May 5, 2019
- *      Author: gazihan
- */
-
-#include "TestedClass.h"
-
---
+++ tests/TestedClass_test.cpp
@@ -0,0 +1,52 @@
+#include "../src/TestedClass.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+// The fixture for testing class Foo.
+class TestedClassTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+
+  TestedClassTest() {
+     // You can do set-up work for each test here.
+  }
+
+  ~TestedClassTest() override {
+     // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  void SetUp() override {
+     // Code here will be called immediately after the constructor (right
+     // before each test).
+  }
+
+  void TearDown() override {
+     // Code here will be called immediately after each test (right
+     // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+
+// Tests that the Foo::Bar() method does Abc.
+TEST_F(TestedClassTest, CanRunTests) {
+	TestedClass tc;
+	EXPECT_EQ(0, 0);
+}
+
+// Tests that Foo does Xyz.
+//TEST_F(TestedClassTest, DoesXyz) {
+//  // Exercises the Xyz feature of Foo.
+//}
+
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
